ecd40d47f7dd75f80554a49bd43dd1cd0812041a
liuxin85 <liuxin85@gmail.com>
2025-04-17 23:57:43 +0800

    feat: integrate clerk

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,16 +1,28 @@
-import { Button } from "@/components/ui/button"
-import { UserCircleIcon } from "lucide-react"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
   // Todo: Add different auth state
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
-      rounded-full shadow-none"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
-  )
-}
+    <>
+      <SignedIn>
+        <UserButton />
+        {/* Add menu items for Studio and User profile */}
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
