9717a2953602236dbf21587e0bbd7dcea4ac6e45
liuxin85 <liuxin85@gmail.com>
2025-04-18 00:06:19 +0800

    feat: test

--- src/app/(home)/protected/something/page.tsx
+++ src/app/(home)/protected/something/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Only logged in users should see this, something routes</div>;
+  return <div>Only logged in users should see this, /proected/something</div>;
 };
 
 export default Page;
