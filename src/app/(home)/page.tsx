bacc449164c62b5557a94ba4bffe088503f81f98
liuxin85 <liuxin85@gmail.com>
2025-04-19 11:32:46 +0800

    feat: trpc configuration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  // const data = await trpc.hello({ text: "Anotoni" });
+  void trpc.hello.prefetch({ text: "Antonio" });
+
   return (
-    <div>
-      <p>I will load videos in the future!</p>
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error... </p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
