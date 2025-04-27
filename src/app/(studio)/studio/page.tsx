2cf13ade3e0bfba495b82c85d99d86c7cbdd5646
liuxin85 <liuxin85@gmail.com>
2025-04-27 08:55:59 +0800

    Studio videos

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,7 +1,18 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 import React from "react";
 
-function page() {
-  return <div>studio</div>;
-}
+const Page = async () => {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
 
-export default page;
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
+};
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,12 @@
+import { studioRouter } from "@/modules/studio/server/procedures";
 import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { videosRouter } from "@/modules/videos/server/procedures";
 
 export const appRouter = createTRPCRouter({
+  studio: studioRouter,
   categories: categoriesRouter,
+  videos: videosRouter,
 });
 
 export type AppRouter = typeof appRouter;
